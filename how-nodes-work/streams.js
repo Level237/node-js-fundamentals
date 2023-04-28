d36bdb46b4b53fd9e245f74836b8ee465f8ac09a
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-04-28 09:50:21 +0100

    stream in pratice

--- how-nodes-work/streams.js
+++ how-nodes-work/streams.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const server = require('http').createServer();
 
-server.on("request",(req,res)=>{
+server.on("request", (req, res) => {
     // // Solution 1
     // fs.readFile('./how-nodes-work/text-file.txt',(err,data)=>{
     //     if(err) console.log(err);
@@ -21,8 +21,13 @@ server.on("request",(req,res)=>{
     //     res.statusCode=500;
     //     res.end("File Not Found");
     // })
+
+    // Solution 3
+    const readable = fs.createReadStream('./how-nodes-work/text-file.txt');
+    
+    // readableSource.pipe(WritableDest)
 })
 
-server.listen(8000,'127.0.0.1',()=>{
+server.listen(8000, '127.0.0.1', () => {
     console.log("Listening....");
 })
