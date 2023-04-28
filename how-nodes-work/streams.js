634e7e4abc986d16e3dec96d1a44202b8e05e3e9
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-04-28 07:35:13 +0100

    Solution 3:using stream

---
+++ how-nodes-work/streams.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on("request",(req,res)=>{
+    // // Solution 1
+    // fs.readFile('./how-nodes-work/text-file.txt',(err,data)=>{
+    //     if(err) console.log(err);
+    //     res.end(data);
+    // })
+
+    // //Solution 2 : Streams
+    // const readable=fs.createReadStream('./how-nodes-work/text-file.txt')
+    // readable.on('data',chunck=>{
+    //     res.write(chunck);
+    // })
+    // readable.on('end',()=>{
+    //     res.end();
+    // })
+    // readable.on('error',err=>{
+    //     console.log(err);
+    //     res.statusCode=500;
+    //     res.end("File Not Found");
+    // })
+})
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log("Listening....");
+})
