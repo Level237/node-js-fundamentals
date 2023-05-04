1a4b344449b78b21be33f000c565f53c03e0f495
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-05-04 18:09:04 +0100

    building Promises

--- asynchronous-js/index.js
+++ asynchronous-js/index.js
@@ -1,18 +1,45 @@
 const fs=require('fs');
 const superagent=require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err,data)=>{
+const readFilePro=file =>{
+    return new Promise((resolve,reject) =>{
+        fs.readFile(file,(err,data) =>{
+            if(err) reject("I could not find that file")
+            resolve(data);
+        })
+    });
+}
+
+const writeFilePro=(file,data) =>{
+
+    return new Promise((resolve,reject) =>{
+        fs.writeFile(file,data,err=>{
+            if(err) reject("I could not write file");
+            resolve('success');
+        })
+    })
+}
+readFilePro(`${__dirname}/dog.txt`).then(data=>{
     console.log(`Breed: ${data}`);
 
-    superagent
+    return superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then(res=>{
+}).then(res=>{
         console.log(res.body.message);
 
-        fs.writeFile('dog-img.txt',res.body.message,err=>{
-            if(err) return console.log(err.message);
-            console.log("Rondom dog image saved to file!");
-        });
+        return writeFilePro('dog-img.txt',res.body.message)
+        
+        // fs.writeFile('dog-img.txt',res.body.message,err=>{
+        //     if(err) return console.log(err.message);
+        //     console.log("Rondom dog image saved to file!");
+        // });
     })
-    .catch(err=>console.log(err.message))
-});
+    .then(() =>{
+        console.log("Random dog image saved to file!");
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+
+
+
